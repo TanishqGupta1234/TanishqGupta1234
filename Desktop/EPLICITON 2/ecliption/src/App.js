9b067f759c9ee12ff2de7f4728c68ee28034f790
TanishqGupta1234 <vandanabhay1997@gmail.com>
2024-05-29 12:43:09 +0530

    Your commit message here

---
+++ Desktop/EPLICITON 2/ecliption/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./EcliptVariant.css";
+// import './App.css';
+import EcliptVariant from "./EcliptVariant";
+
+function App() {
+  <EcliptVariant />
+
+
+
+}
+
+export default App;
---
+++ Desktop/EPLICITON 2/ecliption/src/EcliptVariant.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+import "./EcliptVariant.css";
+
+const EcliptVariant = () => {
+  return (
+    <header className="ecliptvariant3" id="Header" Header="1">
+      <Button className="ecliptvariant3-child" variant="outline-primary">
+        E
+      </Button>
+      <div className="ecliptvariant3-item" />
+      <div className="ecliptvariant3-inner" />
+      <div className="line-div" />
+      <div className="ecliptvariant3-child1" />
+      <div className="ecliptvariant3-child2" />
+      <div className="ecliptvariant3-child3" />
+      <div className="ecliptvariant3-child4" />
+      <div className="ecliptvariant3-child5" />
+      <div className="ecliptvariant3-child6" />
+      <div className="eclipticon">Eclipticon</div>
+      <div className="eclipticon1">{`ECLIPTICON `}</div>
+      <div className="where-every-click">
+        Where every click, swipe, and tap is an experience in itself.
+      </div>
+      <div className="welcome-to">Welcome to</div>
+      <div className="ecliptvariant3-child7" />
+      <img className="white-logo-icon" alt="" src="/white-logo.svg" />
+      <div className="products-wrapper">
+        <div className="products"> Home</div>
+      </div>
+      <div className="products-container">
+        <div className="products1">Products</div>
+      </div>
+      <div className="products-frame">
+        <div className="products1">Consulting</div>
+      </div>
+      <div className="rectangle-div">
+        <div className="products1">Company</div>
+      </div>
+      <div className="products-wrapper1">
+        <div className="products4">Services</div>
+      </div>
+    </header>
+  );
+};
+
+export default EcliptVariant;
